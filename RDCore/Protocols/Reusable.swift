aea02089fc36191f4ade3e08026b76680cce8d55
Yoana Rios <yrrios13@gmail.com>
2021-01-22 23:31:29 -0500

    Initial commit + setup project files

---
+++ RDCore/Protocols/Reusable.swift
@@ -0,0 +1,9 @@
+//
+//  Reusable.swift
+//  RDCore
+//
+//  Created by Yoana Rios  on 2021-01-22.
+//  Copyright © 2021 Yoana R. Rios Diaz. All rights reserved.
+//
+
+import Foundation
---
+++ RDCore/RDCore.h
@@ -0,0 +1,19 @@
+//
+//  RDCore.h
+//  RDCore
+//
+//  Created by Yoana R. Rios Diaz on 2020-09-12.
+//  Copyright © 2020 Yoana R. Rios Diaz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RDCore.
+FOUNDATION_EXPORT double RDCoreVersionNumber;
+
+//! Project version string for RDCore.
+FOUNDATION_EXPORT const unsigned char RDCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RDCore/PublicHeader.h>
+
+
